67c82cedc15db9edd4f050532f66bb82b3a31b65
soumin mohanty <soummohanty@deloitte.com>
2023-11-13 16:56:06 +0530

    Netlfix gpt part 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-green-800 ">
-      Namaste Everyone, lets build Netflix-GPT
+      Namaste Everyone, lets build Netflix-GPT together
     </div>
   );
 }
